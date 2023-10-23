cb3e2193bd770462316f984c02e7fe10ee467334
Lotfi Bouakkaz <bouakkaz144.lotfi@gmail.com>
2023-10-23 14:53:17 +0100

    Api income and expenses endpoints

---
+++ src/api/ExpensesApi.js
@@ -0,0 +1,25 @@
+import axios from 'axios';
+import { BACKEND_URL } from '../api';
+
+const BaseUrl = BACKEND_URL + '/expenses';
+
+export const getExpenses = async () => {
+  const response = await axios.get(`${BaseUrl}`);
+  console.log(response.data);
+  return response.data;
+};
+
+export const addExpense = async (expense) => {
+  const response = await axios.post(`${BaseUrl}/create`, expense);
+  return response.data;
+};
+
+export const deleteExpense = async (expenseId) => {
+  const response = await axios.delete(`${BaseUrl}/delete/${expenseId}`);
+  return response.data;
+};
+
+export const getTotalExpense = async () => {
+  const response = await axios.get(`${BaseUrl}/total`);
+  return response.data;
+};
---
+++ src/api/IncomeApi.js
@@ -0,0 +1,26 @@
+import axios from 'axios';
+import { BACKEND_URL } from '../api';
+
+const BaseUrl = BACKEND_URL + '/income';
+
+export const getIncomes = async () => {
+  const response = await axios.get(`${BaseUrl}`);
+  console.log(response.data);
+  return response.data;
+};
+
+export const addIncome = async (income) => {
+  const response = await axios.post(`${BaseUrl}/create`, income);
+  return response.data;
+};
+
+export const deleteIncome = async (incomeId) => {
+  const response = await axios.delete(`${BaseUrl}/delete/${incomeId}`);
+  return response.data;
+};
+
+// get total income
+export const getTotalIncome = async () => {
+  const response = await axios.get(`${BaseUrl}/total`);
+  return response.data;
+};
