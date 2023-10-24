5eb34a913175cbcb2604e60a6057dc80f4b2842a
Lotfi Bouakkaz <bouakkaz144.lotfi@gmail.com>
2023-10-24 16:02:01 +0100

    sidebar

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,9 +1,10 @@
+import { Avatar } from '@mantine/core';
 import { IconUser, IconWallet } from '@tabler/icons-react';
 
 export function Dashboard({ data, active, setActive }) {
   const links = data.map((item) => (
     <a
-      className={`relative flex flex-row items-center h-11 focus:outline-none hover:bg-gray-50 text-gray-600 hover:text-gray-800  pr-6 ${
+      className={`relative flex flex-row items-center h-11 focus:outline-none hover:bg-gray-50 text-gray-600 hover:text-gray-800 rounded-xl pr-6 ${
         item.link === active ? 'bg-indigo-100 text-indigo-700' : ''
       }`}
       href={item.link}
@@ -21,23 +22,22 @@ export function Dashboard({ data, active, setActive }) {
   ));
 
   return (
-    <div className=' h-screen flex flex-col  bg-white  border-r'>
-      <div className='flex items-center justify-center h-14 border-b text-orange-500'>
-        <IconWallet className='mr-2 ' />
-        <h1 className='text-2xl font-semibold'>Dinero</h1>
+    <div className=' h-screen flex flex-col px-12  bg-white  border-r'>
+      <div className='flex flex-col justify-center items-center mt-20'>
+        <Avatar size='xl' src={null} alt='no image here' />
+        <p className='text-gray-400 mt-3'>Welcome,</p>
+        <p className='font-semibold'>Lotfi Bouakkaz</p>
       </div>
 
-      <div className='flex flex-col  pt-4 '>
-        <div>
-          <div className='px-5'>
-            <div className='flex flex-row items-center h-8'>
-              <div className='text-sm font-light tracking-wide text-gray-500'>
-                Menu
-              </div>
+      <div className='flex flex-col mt-6 pt-4 '>
+        <div className='px-5'>
+          <div className='flex flex-row items-center h-8'>
+            <div className='text-sm font-light tracking-wide text-gray-500'>
+              Menu
             </div>
           </div>
-          {links}
         </div>
+        {links}
       </div>
       <div className='mt-auto border-t py-4 '>
         <div className='px-5 '>
