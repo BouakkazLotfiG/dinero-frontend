ae373c1e2ae12f854ab4a832bfc61f8e016f543a
Lotfi Bouakkaz <bouakkaz144.lotfi@gmail.com>
2023-10-26 18:57:41 +0100

    Sidebar responsive

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -4,7 +4,7 @@ import { IconUser, IconWallet } from '@tabler/icons-react';
 export function Dashboard({ data, active, setActive }) {
   const links = data.map((item) => (
     <a
-      className={`relative flex flex-row items-center h-11 focus:outline-none hover:bg-indigo-100 text-gray-600 hover:text-indigo-700 transition ease-in-out duration-200  rounded-md pr-6 ${
+      className={`relative flex flex-row justify-center md:justify-start items-center h-11 focus:outline-none hover:bg-indigo-100 text-gray-600 hover:text-indigo-700 transition ease-in-out duration-200  rounded-md md:pr-6 ${
         item.link === active ? 'bg-indigo-100 text-indigo-700' : ''
       }`}
       href={item.link}
@@ -14,23 +14,25 @@ export function Dashboard({ data, active, setActive }) {
         setActive(item.link);
       }}
     >
-      <span className='inline-flex justify-center items-center ml-4'>
-        <item.icon className='mr-2' />
+      <span className='inline-flex justify-center items-center md:ml-4'>
+        <item.icon className='md:mr-2' />
       </span>
-      <span className='ml-2 text-sm tracking-wide truncate'>{item.label}</span>
+      <span className='hidden md:block  ml-2 text-sm tracking-wide truncate'>
+        {item.label}
+      </span>
     </a>
   ));
 
   return (
-    <div className=' h-screen flex flex-col px-12  bg-white  border-r'>
-      <div className='flex flex-col justify-center items-center mt-20'>
+    <div className=' h-screen flex flex-col px-2 md:px-4 lg:px-12  bg-white  border-r'>
+      <div className='hidden md:flex flex-col justify-center items-center mt-20'>
         <Avatar size='xl' src={null} alt='no image here' />
         <p className='text-gray-400 mt-3'>Welcome,</p>
         <p className='font-semibold'>Lotfi Bouakkaz</p>
       </div>
 
       <div className='flex flex-col mt-6 pt-4 '>
-        <div className='px-5'>
+        <div className='mx-auto md:px-5'>
           <div className='flex flex-row items-center h-8'>
             <div className='text-sm font-light tracking-wide text-gray-500'>
               Menu
@@ -40,7 +42,7 @@ export function Dashboard({ data, active, setActive }) {
         <div className='flex flex-col gap-2'>{links}</div>
       </div>
       <div className='mt-auto border-t py-4 '>
-        <div className='px-5 '>
+        <div className='mx-auto md:px-5'>
           <div className='flex flex-row items-center h-8'>
             <div className='text-sm font-light tracking-wide text-gray-500'>
               Settings
